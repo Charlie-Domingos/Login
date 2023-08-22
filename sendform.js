d92760d4079dc05bf82063f3f8f3c3e7d5f12f15
charlie-harpe <charlie.augusto@outlook.com>
2023-08-22 17:02:39 -0300

    athentication

--- sendform.js
+++ sendform.js
@@ -1,54 +1,62 @@
 // Coleta inputs do login-form
-document.addEventListener("DOMContentLoaded", function() {
-  const loginForm = document.getElementById("login-form");
-  const user = [];
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.getElementById('login-form')
+  const user = []
 
-  loginForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    
+  var validEmail = 'charliedev022@gmail.com'
+  var validPassword = 'teste'
+
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
     const oldUser = {
       email: email,
-      password: password,
-    };
+      password: password
+    }
 
-    user.push(oldUser);
+    user.push(oldUser)
 
-    alert(`Seja bem vindo ${email}!`)
-    console.log(user);
+    console.log(user)
 
-    document.getElementById("email").value = "";
-    document.getElementById("password").value = "";
-  });
-});
+    document.getElementById('email').value = ''
+    document.getElementById('password').value = ''
+
+    if (email === validEmail && validPassword === password) {
+      window.location.href = 'home.html'
+    } else {
+      alert('Email do usuário ou senha incorretos.')
+    }
+  })
+})
 
 // Coleta inputs do register-form
 
-document.addEventListener("DOMContentLoaded", function () {
-  const cadastroForm = document.getElementById("register-form");
-  const usuario = [];
+document.addEventListener('DOMContentLoaded', function () {
+  const cadastroForm = document.getElementById('register-form')
+  const usuario = []
 
-  cadastroForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const nome = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  cadastroForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const nome = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
 
     const newUsuario = {
       name: nome,
       email: email,
-      password: password,
-    };
+      password: password
+    }
 
-    usuario.push(newUsuario);
+    usuario.push(newUsuario)
 
     alert(`Usuário ${nome} cadastrado com sucesso!`)
 
-    console.log(usuario);
+    console.log(usuario)
 
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("password").value = "";
-  });
-});
+    document.getElementById('name').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('password').value = ''
+  })
+})
